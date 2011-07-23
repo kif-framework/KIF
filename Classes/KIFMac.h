c0e74192b2446f4951794ebe9e31fd35d165adba
joshaber <joshaber@gmail.com>
2011-07-23 15:59:05 -0700

    include a framework header for mac

---
+++ Classes/KIFMac.h
@@ -0,0 +1,15 @@
+//
+//  KIFMac.h
+//  KIF
+//
+//  Created by Josh Abernathy on 7/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <KIF/KIFTestController.h>
+#import <KIF/KIFTestScenario.h>
+#import <KIF/KIFTestStep.h>
+#import <KIF/KIFTestStep-Mac.h>
+
+#import <KIF/KIFApplication.h>
+#import <KIF/KIFElement.h>
