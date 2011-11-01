031d67b548e4d210d02d4c90ecbaac206b7b829f
Eric Firestone <firestone@squareup.com>
2011-11-01 00:34:18 -0700

    Add steps to set up and tear down > > This adds an easy way to set common steps for setting up and tearing down a scenario. Common steps should be set using the "default" class methods, and can be overridden on an individual scenario instance if desired.
    Thanks to toddlee (https://github.com/square/KIF/pull/59) for the initial implementation and direction.

--- Classes/KIFTestScenario.h
+++ Classes/KIFTestScenario.h
@@ -21,6 +21,8 @@
  */
 @interface KIFTestScenario : NSObject {
     NSMutableArray *steps;
+    NSMutableArray *stepsToSetUp;
+    NSMutableArray *stepsToTearDown;
     NSString *description;
     BOOL skippedByFilter;
 }
@@ -28,16 +30,29 @@
 /*!
  @property description
  @abstract A description of what the scenario tests.
- @discussion This should be a thorough description of what the scenario is testing so that if the test fails, it is clear which test it was.
+ @discussion This should be a thorough description of what the scenario is testing so that if the test fails it is clear which test it was.
  */
 @property (nonatomic, retain) NSString *description;
 
 /*!
  @property steps
  @abstract The steps that comprise the scenario.
- @discussion The steps are instances of KIFTestStep (or a subclass).
+ @discussion The steps are instances of KIFTestStep (or a subclass). This method returns all steps, including the steps to set up and the steps to tear down.
  */
 @property (nonatomic, readonly, retain) NSArray *steps;
+
+/*!
+ @property stepsToSetUp
+ @abstract Steps that will be executed at the beginning of the scenario.
+ @discussion The steps to set up are an array of KIFTestStep (or subclass) instances that will be executed at the beginning of the scenario, before the steps specified in the -steps property. When initializing the scenario these steps are defaulted to the steps specified by +defaultStepsToSetUp, but may be overridden by setting them directly using this property.
+ */
+@property (nonatomic, copy) NSArray *stepsToSetUp;
+
+/*!
+ @property stepsToTearDown
+ @abstract Steps that will be executed at the end of the scenario.
+ @discussion The steps to tear down are an array of KIFTestStep (or subclass) instances that will be executed at the end of the scenario, after the steps specified in the -steps property. When initializing the scenario these steps are defaulted to the steps specified by +defaultStepsToTearDown, but may be overridden by setting them directly using this property.
+ */@property (nonatomic, copy) NSArray *stepsToTearDown;
 
 /*!
  @property skippedByFilter
@@ -54,6 +69,38 @@
  @discussion Creates a new instance of the scenario with a given description. As part of creating the instance, @link initializeSteps initializeSteps @/link will be called, so calling this method on a subclass of KIFTestScenario will return a fully initialized scenario.
  */
 + (id)scenarioWithDescription:(NSString *)description;
+
+/*!
+ @method setDefaultStepsToSetUp:
+ @abstract Set the default setup steps that will be added to new scenarios.
+ @param The default setup steps.
+ @discussion When initializing a new scenario these steps are set as the stepsToSetUp on the scenario.
+ */
++ (void)setDefaultStepsToSetUp:(NSArray *)steps;
+
+/*!
+ @method defaultStepsToSetUp
+ @abstract The default setup steps that will be added to new scenarios.
+ @result The default setup steps.
+ @discussion When initializing a new scenario these steps are set as the stepsToSetUp on the scenario. These default steps can be set using +setDefaultStepsToSetUp:
+ */
++ (NSArray *)defaultStepsToSetUp;
+
+/*!
+ @method setDefaultStepsToTearDown:
+ @abstract Set the default tear down steps that will be added to new scenarios.
+ @param The default tear down steps.
+ @discussion When initializing a new scenario these steps are set as the stepsToTearDown on the scenario.
+ */
++ (void)setDefaultStepsToTearDown:(NSArray *)steps;
+
+/*!
+ @method defaultStepsToTearDown
+ @abstract The default tear down steps that will be added to new scenarios.
+ @result The default tear down steps.
+ @discussion When initializing a new scenario these steps are set as the stepsToTearDown on the scenario. These default steps can be set using +setDefaultStepsToTearDown:
+ */
++ (NSArray *)defaultStepsToTearDown;
 
 /*!
  @method initializeSteps;
--- Classes/KIFTestScenario.m
+++ Classes/KIFTestScenario.m
@@ -10,6 +10,11 @@
 #import "KIFTestScenario.h"
 #import "KIFTestStep.h"
 
+
+static NSArray *defaultStepsToSetUp = nil;
+static NSArray *defaultStepsToTearDown = nil;
+
+
 @interface KIFTestScenario ()
 
 @property (nonatomic, readwrite, retain) NSArray *steps;
@@ -19,10 +24,13 @@
 
 @end
 
+
 @implementation KIFTestScenario
 
 @synthesize description;
 @synthesize steps;
+@synthesize stepsToSetUp;
+@synthesize stepsToTearDown;
 @synthesize skippedByFilter;
 
 #pragma mark Static Methods
@@ -35,7 +43,38 @@
     if (filter) {
         scenario.skippedByFilter = ([description rangeOfString:filter options:NSRegularExpressionSearch].location == NSNotFound);
     }
+    
     return [scenario autorelease];
+}
+
++ (void)setDefaultStepsToSetUp:(NSArray *)steps;
+{
+    if (defaultStepsToSetUp == steps) {
+        return;
+    }
+    
+    [defaultStepsToSetUp release];
+    defaultStepsToSetUp = [steps copy];
+}
+
++ (NSArray *)defaultStepsToSetUp;
+{
+    return defaultStepsToSetUp;
+}
+
++ (void)setDefaultStepsToTearDown:(NSArray *)steps;
+{
+    if (defaultStepsToTearDown == steps) {
+        return;
+    }
+    
+    [defaultStepsToTearDown release];
+    defaultStepsToTearDown = [steps copy];
+}
+
++ (NSArray *)defaultStepsToTearDown;
+{
+    return defaultStepsToTearDown;
 }
 
 #pragma mark Initialization
@@ -47,13 +86,18 @@
         return nil;
     }
     
+    stepsToSetUp = [defaultStepsToSetUp copy];
+    stepsToTearDown = [defaultStepsToTearDown copy];
+    
     return self;
 }
 
 - (void)dealloc
 {
-    self.steps = nil;
-    self.description = nil;
+    [steps release]; steps = nil;
+    [stepsToSetUp release]; stepsToSetUp = nil;
+    [stepsToTearDown release]; stepsToTearDown = nil;
+    [description release]; description = nil;
     
     [super dealloc];
 }
@@ -76,7 +120,7 @@
     NSAssert(![steps containsObject:step], @"The step %@ is already added", step);
     
     [self _initializeStepsIfNeeded];
-    [steps addObject:step];
+    [steps insertObject:step atIndex:(steps.count - self.stepsToTearDown.count)];
 }
 
 - (void)addStepsFromArray:(NSArray *)inSteps;
@@ -86,15 +130,47 @@
     }
     
     [self _initializeStepsIfNeeded];
-    [steps addObjectsFromArray:inSteps];
+    [steps insertObjects:inSteps atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(steps.count - self.stepsToTearDown.count, inSteps.count)]];
+}
+
+- (void)setStepsToSetUp:(NSArray *)inStepsToSetUp;
+{
+    if ([stepsToSetUp isEqual:inStepsToSetUp]) {
+        return;
+    }
+    
+    // Remove the old set up steps and add the new ones
+    // If steps hasn't been set up yet, that's fine
+    [steps removeObjectsInRange:NSMakeRange(0, stepsToSetUp.count)];
+    [steps insertObjects:inStepsToSetUp atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, inStepsToSetUp.count)]];
+    
+    [stepsToSetUp release];
+    stepsToSetUp = [inStepsToSetUp copy];
+}
+
+- (void)setStepsToTearDown:(NSArray *)inStepsToTearDown;
+{
+    if ([stepsToTearDown isEqual:inStepsToTearDown]) {
+        return;
+    }
+        
+    // Remove the old tear down steps and add the new ones
+    // If steps hasn't been set up yet, that's fine
+    [steps removeObjectsInRange:NSMakeRange(steps.count - stepsToTearDown.count, stepsToTearDown.count)];
+    [steps insertObjects:inStepsToTearDown atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(steps.count, inStepsToTearDown.count)]];
+    
+    [stepsToTearDown release];
+    stepsToTearDown = [inStepsToTearDown copy];
 }
 
 #pragma mark Private Methods
 
-- (void)_initializeStepsIfNeeded
+- (void)_initializeStepsIfNeeded;
 {
     if (!steps && !self.skippedByFilter) {
-        self.steps = [NSMutableArray array];
+        NSMutableArray *initialSteps = [NSMutableArray arrayWithArray:self.stepsToSetUp];
+        [initialSteps addObjectsFromArray:self.stepsToTearDown];
+        self.steps = initialSteps;
         [self initializeSteps];
     }
 }
