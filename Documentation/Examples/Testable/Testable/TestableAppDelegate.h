6238745297df9bb56711f1afe6964a5057db658a
Jim Puls <puls@squareup.com>
2011-11-14 18:26:47 -0800

    Make Testable use ARC to show that KIF works with ARC-enabled apps; fix the one ARC incompatibility in KIF.

--- Documentation/Examples/Testable/Testable/TestableAppDelegate.h
+++ Documentation/Examples/Testable/Testable/TestableAppDelegate.h
@@ -13,7 +13,7 @@
 
 }
 
-@property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+@property (nonatomic, strong) IBOutlet UINavigationController *navigationController;
 
 @end
