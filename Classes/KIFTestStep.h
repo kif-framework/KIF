4b23575d101fa7b4ee5f5d960ddd9c64cc46e6a6
Jake Marsh <jake@thejakemarsh.com>
2012-01-16 22:54:37 -0600

    Added two new methods to verify the number of sections, and the number of rows in a section, of a given UITableView.

--- Classes/KIFTestStep.h
+++ Classes/KIFTestStep.h
@@ -395,4 +395,25 @@ typedef KIFTestStepResult (^KIFTestStepExecutionBlock)(KIFTestStep *step, NSErro
  */
 + (id)stepToTapRowInTableViewWithAccessibilityLabel:(NSString*)tableViewLabel atIndexPath:(NSIndexPath *)indexPath;
 
+/*!
+ @method stepToVerifyNumberOfSections:inTableViewWithAccessibilityLabel:
+ @abstract A step that verifies the number of sections in a table view with a given label.
+ @discussion This step will get the view with the specified accessibility label and and compare it's number of sections with the value passed in.
+ @param sections The number of sections the table view should be verified to have
+ @param tableViewLabel Accessibility label of the table view.
+ @result A configured test step.
+ */
++ (id) stepToVerifyNumberOfSections:(NSInteger)sections inTableViewWithAccessibilityLabel:(NSString *)tableViewLabel;
+
+/*!
+ @method stepToVerifyNumberOfRows:inSection:ofTableViewWithAccessibilityLabel:
+ @abstract A step that verifies the number of rows in a section at the given index in a table view with a given label.
+ @discussion This step will get the view with the specified accessibility label and verify if the number of rows in the given section matches the values passed in.
+ @param rows The number of rows the given section of the table view should be verified to have
+ @param section The index of the section to verify the row count of
+ @param tableViewLabel Accessibility label of the table view.
+ @result A configured test step.
+ */
++ (id) stepToVerifyNumberOfRows:(NSInteger)rows inSection:(NSInteger)section ofTableViewWithAccessibilityLabel:(NSString *)tableViewLabel;
+
 @end
