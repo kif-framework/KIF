e73e5a806b3ae1e4289a9a8feb6c47e21cdc34a5
William Tan <william@z2live.com>
2012-10-01 14:15:39 -0700

    Removed lingering method prototype as a result of privatizing the method

--- Classes/KIFTestStep.h
+++ Classes/KIFTestStep.h
@@ -335,21 +335,6 @@ typedef KIFTestStepResult (^KIFTestStepExecutionBlock)(KIFTestStep *step, NSErro
 
 /*!
  Z2Live Addition
- @method stepToTapViewWithAccessibilityLabel:value:traits:
- @abstract A step that taps a particular view in the view hierarchy.
- @discussion The view or accessibility element with the given label is searched for in the view hierarchy. If the element isn't found or isn't currently tappable, then the step will attempt to wait until it is. Once the view is present and tappable, a tap event is simulated in the center of the view or element.
- 
- This variation allows attempting to tap a label if it exists, and will not error on failure
- @param label The accessibility label of the element to tap.
- @param value The accessibility value of the element to tap.
- @param traits The accessibility traits of the element to tap. Elements that do not include at least these traits are ignored.
- @param successResultOnFailure If YES, then the test will return a KIFTestStepSuccess at all times. If it cannot tap the view, it will return KIFTestStepFailure
- @result A configured test step.
- */
-+ (id)stepToTapViewWithAccessibilityLabel:(NSString *)label value:(NSString *)value traits:(UIAccessibilityTraits)traits successResultOnFailure:(BOOL)successResultOnFailure;
-
-/*!
- Z2Live Addition
  @method stepToTapViewIfExistsWithAccessibilityLabel:
  @abstract A step that taps a particular view in the view hierarchy, if it exists
  @discussion The view or accessibility element with the given label is searched for in the view hierarchy. If the element isn't found or isn't currently tappable, then the step will attempt to wait until it is. Once the view is present and tappable, a tap event is simulated in the center of the view or element. If the view does not exist, it will return success.
