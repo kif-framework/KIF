afb761ced6a1e23744c4ae60e32795ff48d53cce
jules2689 <julian.nadeau@shopify.com>
2013-02-22 11:48:17 -0500

    Add direction to swipePathForFlick as per PR.

--- Classes/KIFTestStep.h
+++ Classes/KIFTestStep.h
@@ -566,7 +566,8 @@ typedef enum {
  @abstract A method to get a swipePath for flicking.
  @discussion The swipe path is found by determining points between 2 end points.
  @param The view to swipe, used for determining the points.
+  @param The direction to swipe.
  @result A path of points, a point array.
  */
-+ (CGPoint*)swipePathForFlick:(UIView *)viewToSwipe;
++ (CGPoint*)swipePathForFlick:(UIView *)viewToSwipe direction:(KIFSwipeDirection)direction;
 @end
