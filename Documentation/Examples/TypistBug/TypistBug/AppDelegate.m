8b54b14c6452fec8934622300de0766a4f1cacaf
Pete Hodgson <git@thepete.net>
2013-03-20 08:47:11 -0700

    add initial test screen

--- Documentation/Examples/TypistBug/TypistBug/AppDelegate.m
+++ Documentation/Examples/TypistBug/TypistBug/AppDelegate.m
@@ -16,11 +16,8 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil];
-    } else {
-        self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil];
-    }
+
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
--- Documentation/Examples/TypistBug/TypistBug/ViewController.m
+++ Documentation/Examples/TypistBug/TypistBug/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *exampleTextField;
+@property (weak, nonatomic) IBOutlet UISwitch *capitalizationSwitch;
 
+- (void) updateCapitalization;
 @end
 
 @implementation ViewController
@@ -18,12 +21,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self updateCapitalization];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)capitalizationDidChange:(id)sender {
+    [self updateCapitalization];
+}
+
+- (void) updateCapitalization{
+    if( self.capitalizationSwitch.on ){
+        self.exampleTextField.autocapitalizationType = UITextAutocapitalizationTypeWords;
+    }else{
+        self.exampleTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    }
 }
 
 @end
