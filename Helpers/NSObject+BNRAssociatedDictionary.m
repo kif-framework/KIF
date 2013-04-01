b75ccf81872d722d2f9d0e7bde9d6edf4e0140b2
Nate Chandler <nathaniel.chandler@gmail.com>
2013-04-01 17:16:24 -0400

    Added NSObject+BNRAssociatedDictionary.

---
+++ Helpers/NSObject+BNRAssociatedDictionary.m
@@ -0,0 +1,52 @@
+//
+//  NSObject+BNRAssociatedDictionary.m
+//  KIF
+//
+//  Created by Nate Chandler on 3/27/13.
+//
+//
+
+#import "NSObject+BNRAssociatedDictionary.h"
+#import <objc/runtime.h>
+
+@implementation NSObject (BNRAssociatedDictionary)
+
+static char NSObjectADDLAssociatedDictionaryAssociatedDictionaryAssociatedObjectKey = 0;
+
+- (NSMutableDictionary *)bnr_associatedDictionaryPrimitive
+{
+	return objc_getAssociatedObject(self, &NSObjectADDLAssociatedDictionaryAssociatedDictionaryAssociatedObjectKey);
+}
+
+- (void)bnr_setAssociatedDictionaryPrimitive:(NSMutableDictionary *)associatedDictionary
+{
+	objc_setAssociatedObject(self, &NSObjectADDLAssociatedDictionaryAssociatedDictionaryAssociatedObjectKey, associatedDictionary, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSMutableDictionary *)bnr_generateAssociatedDictionary
+{
+	NSMutableDictionary *res = [[NSMutableDictionary alloc] init];
+	[self bnr_setAssociatedDictionaryPrimitive:res];
+	return res;
+}
+
+- (NSMutableDictionary *)bnr_associatedDictionary
+{
+	NSMutableDictionary *res = [self bnr_associatedDictionaryPrimitive];
+	if (!res) {
+		res = [self bnr_generateAssociatedDictionary];
+	}
+	return res;
+}
+
+- (id)bnr_associatedObjectForKey:(id<NSCopying>)key
+{
+	return self.bnr_associatedDictionary[key];
+}
+
+- (void)bnr_setAssociatedObject:(id)object forKey:(id<NSCopying>)key
+{
+	self.bnr_associatedDictionary[key] = object;
+}
+
+@end
