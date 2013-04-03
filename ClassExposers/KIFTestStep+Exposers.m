61bbb688a09ffbb56a87b52e11eca322ee9d994e
Nate Chandler <nathaniel.chandler@gmail.com>
2013-04-03 18:20:38 -0400

    Added header exposing KIFTestStep properties.

---
+++ ClassExposers/KIFTestStep+Exposers.m
@@ -0,0 +1,19 @@
+//
+//  KIFTestStep+Exposers.m
+//  KIF
+//
+//  Created by Nate Chandler on 4/3/13.
+//
+//
+
+#import "KIFTestStep+Exposers.h"
+
+@implementation KIFTestStep (Exposers)
+
+@dynamic executionBlock;
+@dynamic notificationName;
+@dynamic notificationObject;
+@dynamic notificationOccurred;
+@dynamic observingForNotification;
+
+@end
