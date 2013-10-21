233adf363581eebc5ea634c552e62a5aec40fa95
Kra Larivain <olarivain@gmail.com>
2013-10-21 06:27:28 -0700

    - UIBarButtonItem now supports accessibility identifier

---
+++ Additions/UIBarButtonItem-KIFAdditions.m
@@ -0,0 +1,32 @@
+//
+//  UIBarButtonItem+KIFAdditions.m
+//  KIF
+//
+//  Created by Olivier Larivain on 10/18/13.
+//
+//
+
+#import "UIBarButtonItem-KIFAdditions.h"
+
+@implementation UIBarButtonItem (KIFAdditions)
+
+- (NSString *) accessibilityIdentifier {
+	if(self.customView != nil) {
+		return self.customView.accessibilityIdentifier;
+	}
+	
+	UIView *view = [self valueForKey: @"view"];
+	return view.accessibilityIdentifier;
+}
+
+- (void) setAccessibilityIdentifier:(NSString *)accessibilityIdentifier {
+	if(self.customView != nil) {
+		self.customView.accessibilityIdentifier = accessibilityIdentifier;
+		return;
+	}
+	
+	UIView *view = [self valueForKey: @"view"];
+	view.accessibilityIdentifier = accessibilityIdentifier;
+}
+
+@end
