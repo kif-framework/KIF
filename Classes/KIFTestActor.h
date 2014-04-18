0a6c9c22796deca1bf799c7c3f59699d6a33f4f0
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2014-04-18 22:27:33 +0200

    Changed KIFTestStepResult enum into new style NS_ENUM. This change fixes a compilation problem while using XCTool to run a build. Apparently the compiler is unable to determine the correct return type of a block using said enum and causes a "incompatible block pointer types".

--- Classes/KIFTestActor.h
+++ Classes/KIFTestActor.h
@@ -59,12 +59,11 @@ return KIFTestStepResultWait; \
  @constant KIFTestStepResultSuccess The step succeeded and the test controller should move to the next step in the current scenario.
  @constant KIFTestStepResultWait The test isn't ready yet and should be tried again after a short delay.
  */
-enum {
+typedef NS_ENUM(NSUInteger, KIFTestStepResult) {
     KIFTestStepResultFailure = 0,
     KIFTestStepResultSuccess,
     KIFTestStepResultWait,
 };
-typedef NSInteger KIFTestStepResult;
 
 /*!
  @typedef KIFTestExecutionBlock
