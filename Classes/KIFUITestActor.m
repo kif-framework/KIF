ae0a19c7377268ca77ba7ea230e396f7d1e2c85b
Wes Lindsay <wlindsay@shopkeep.com>
2014-07-03 17:44:06 +0100

    Added swipe with accessibility traits method

--- Classes/KIFUITestActor.m
+++ Classes/KIFUITestActor.m
@@ -639,21 +639,26 @@
 
 - (void)swipeViewWithAccessibilityLabel:(NSString *)label inDirection:(KIFSwipeDirection)direction
 {
+    [self swipeViewWithAccessibilityLabel:label traits:UIAccessibilityTraitNone inDirection:direction];
+}
+
+- (void)swipeViewWithAccessibilityLabel:(NSString *)label traits:(UIAccessibilityTraits)traits inDirection:(KIFSwipeDirection)direction
+{
     const NSUInteger kNumberOfPointsInSwipePath = 20;
-    
+
     // The original version of this came from http://groups.google.com/group/kif-framework/browse_thread/thread/df3f47eff9f5ac8c
-    
+
     UIView *viewToSwipe = nil;
     UIAccessibilityElement *element = nil;
-    
-    [self waitForAccessibilityElement:&element view:&viewToSwipe withLabel:label value:nil traits:UIAccessibilityTraitNone tappable:NO];
-    
+
+    [self waitForAccessibilityElement:&element view:&viewToSwipe withLabel:label value:nil traits:traits tappable:NO];
+
     // Within this method, all geometry is done in the coordinate system of the view to swipe.
-    
+
     CGRect elementFrame = [viewToSwipe.windowOrIdentityWindow convertRect:element.accessibilityFrame toView:viewToSwipe];
     CGPoint swipeStart = CGPointCenteredInRect(elementFrame);
     KIFDisplacement swipeDisplacement = KIFDisplacementForSwipingInDirection(direction);
-    
+
     [viewToSwipe dragFromPoint:swipeStart displacement:swipeDisplacement steps:kNumberOfPointsInSwipePath];
 }
 
