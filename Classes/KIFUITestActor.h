268863bb0003890133f1a7f6dbe3b74035c2eb0e
Krutarth <krutarth2951987@gmail.com>
2014-09-03 12:57:06 +1000

    PickerView select row in component method added.

--- Classes/KIFUITestActor.h
+++ Classes/KIFUITestActor.h
@@ -314,6 +314,14 @@ static inline KIFDisplacement KIFDisplacementForSwipingInDirection(KIFSwipeDirec
 - (void)selectPickerViewRowWithTitle:(NSString *)title;
 
 /*!
+ @abstract Selects an item from a currently visible picker view in specified component.
+ @discussion With a picker view already visible, this step will find an item with the given title in given component, select that item, and tap the Done button.
+ @param title The title of the row to select.
+ @param component The component tester inteds to select the title in.
+ */
+- (void)selectPickerViewRowWithTitle:(NSString *)title inComponent:(NSInteger)component;
+
+/*!
  @abstract Selects a value from a currently visible date picker view.
  @discussion With a date picker view already visible, this step will select the different rotating weel values in order of how the array parameter is passed in. After it is done it will hide the date picker. It works with all 4 UIDatePickerMode* modes. The input parameter of type NSArray has to match in what order the date picker is displaying the values/columns. So if the locale is changing the input parameter has to be adjusted. Example: Mode: UIDatePickerModeDate, Locale: en_US, Input param: NSArray *date = @[@"June", @"17", @"1965"];. Example: Mode: UIDatePickerModeDate, Locale: de_DE, Input param: NSArray *date = @[@"17.", @"Juni", @"1965".
  @param datePickerColumnValues Each element in the NSArray represents a rotating wheel in the date picker control. Elements from 0 - n are listed in the order of the rotating wheels, left to right.
