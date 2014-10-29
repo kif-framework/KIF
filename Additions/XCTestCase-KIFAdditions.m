cc3c9038c5275ddbfb6ff90a260dae3db3cad3e1
Brian Nickel <bnickel@stackexchange.com>
2014-10-29 13:12:03 -0700

    Suppress ugly exception log on expected exceptions
    When XCTestCase tries recording the thrown exception, check whether or
    not it’s a “stop” exception.  If so, suppress the logging.  Resolves
    issue #401.

--- Additions/XCTestCase-KIFAdditions.m
+++ Additions/XCTestCase-KIFAdditions.m
@@ -8,8 +8,23 @@
 
 #import "XCTestCase-KIFAdditions.h"
 #import "LoadableCategory.h"
+#import <objc/runtime.h>
 
 MAKE_CATEGORIES_LOADABLE(TestCase_KIFAdditions)
+
+static inline void Swizzle(Class c, SEL orig, SEL new)
+{
+    Method origMethod = class_getInstanceMethod(c, orig);
+    Method newMethod = class_getInstanceMethod(c, new);
+    if(class_addMethod(c, orig, method_getImplementation(newMethod), method_getTypeEncoding(newMethod)))
+        class_replaceMethod(c, new, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
+    else
+        method_exchangeImplementations(origMethod, newMethod);
+}
+
+@interface XCTestCase ()
+- (void)_recordUnexpectedFailureWithDescription:(id)arg1 exception:(id)arg2;
+@end
 
 @implementation XCTestCase (KIFAdditions)
 
@@ -20,6 +35,10 @@ MAKE_CATEGORIES_LOADABLE(TestCase_KIFAdditions)
     [self recordFailureWithDescription:exception.description inFile:exception.userInfo[@"SenTestFilenameKey"] atLine:[exception.userInfo[@"SenTestLineNumberKey"] unsignedIntegerValue] expected:NO];
 
     if (stop) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            Swizzle([XCTestCase class], @selector(_recordUnexpectedFailureWithDescription:exception:), @selector(KIF_recordUnexpectedFailureWithDescription:exception:));
+        });
         [exception raise];
     }
 }
@@ -29,6 +48,13 @@ MAKE_CATEGORIES_LOADABLE(TestCase_KIFAdditions)
     NSException *lastException = exceptions.lastObject;
     for (NSException *exception in exceptions) {
         [self failWithException:exception stopTest:(exception == lastException ? stop : NO)];
+    }
+}
+
+- (void)KIF_recordUnexpectedFailureWithDescription:(id)arg1 exception:(NSException *)arg2
+{
+    if (![[arg2 name] isEqualToString:@"KIFFailureException"]) {
+        [self KIF_recordUnexpectedFailureWithDescription:arg1 exception:arg2];
     }
 }
 
