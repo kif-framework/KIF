1aa1c334a3760d5e2d3a0560488233511a01cae6
Justin Martin <jmartin@squareup.com>
2014-12-12 00:58:25 -0800

    Fix documentation bug

--- Additions/UIApplication-KIFAdditions.h
+++ Additions/UIApplication-KIFAdditions.h
@@ -35,7 +35,7 @@ UIKIT_EXTERN NSString *const UIApplicationOpenedURLKey;
 /*!
  @abstract Finds an accessibility element where @c matchBlock returns @c YES, across all windows in the application starting at the fronmost window.
  @discussion This method should be used if @c accessibilityElementWithLabel:accessibilityValue:traits: does not meet your requirements.  For example, if you are searching for an element that begins with a pattern or if of a certain view type.
- @param matchBlock.  A block to be performed on each element to see if it passes.
+ @param matchBlock  A block to be performed on each element to see if it passes.
  */
 - (UIAccessibilityElement *)accessibilityElementMatchingBlock:(BOOL(^)(UIAccessibilityElement *))matchBlock;
 
