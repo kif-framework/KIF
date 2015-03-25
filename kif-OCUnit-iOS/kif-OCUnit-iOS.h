356ed3202ff2bcb66a584bb9c432d9b8b28fadfc
Simone Manganelli <simX@mac.com>
2015-03-25 14:42:37 -0700

    add framework targets/schemes for Carthage

---
+++ kif-OCUnit-iOS/kif-OCUnit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  kif-OCUnit-iOS.h
+//  kif-OCUnit-iOS
+//
+//  Created by Simone Manganelli on 25/03/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kif-OCUnit-iOS.
+FOUNDATION_EXPORT double kif_OCUnit_iOSVersionNumber;
+
+//! Project version string for kif-OCUnit-iOS.
+FOUNDATION_EXPORT const unsigned char kif_OCUnit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kif_OCUnit_iOS/PublicHeader.h>
+
+
