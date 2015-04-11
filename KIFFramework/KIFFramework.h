cd6f89e6199fc17d21b1a444d4e4ea0499b7d94c
Shane Zatezalo <shane@lottadot.com>
2015-04-11 13:25:09 -0400

    Add KIFFramework for Carthage support.
    * iOS8 Dylib Framework
    * Add Carthage declaration of compatibility to README
    * Set KIF Framework deployment target 8.0

---
+++ KIFFramework/KIFFramework.h
@@ -0,0 +1,41 @@
+//
+//  KIFFramework.h
+//  KIFFramework
+//
+//  Created by Shane Zatezalo on 4/7/15.
+//  Copyright (c) 2015 Lottadot LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+
+//! Project version number for KIFFramework.
+FOUNDATION_EXPORT double KIFFrameworkVersionNumber;
+
+//! Project version string for KIFFramework.
+FOUNDATION_EXPORT const unsigned char KIFFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KIFFramework/PublicHeader.h>
+
+#import <KIFFramework/KIF.h>
+#import <KIFFramework/KIFTestActor.h>
+#import <KIFFramework/KIFTestCase.h>
+#import <KIFFramework/KIFSystemTestActor.h>
+#import <KIFFramework/KIFUITestActor.h>
+#import <KIFFramework/KIFUITestActor-ConditionalTests.h>
+#import <KIFFramework/CGGeometry-KIFAdditions.h>
+#import <KIFFramework/KIFTestStepValidation.h>
+#import <KIFFramework/KIFTypist.h>
+#import <KIFFramework/LoadableCategory.h>
+#import <KIFFramework/NSBundle-KIFAdditions.h>
+#import <KIFFramework/NSError-KIFAdditions.h>
+#import <KIFFramework/NSException-KIFAdditions.h>
+#import <KIFFramework/NSFileManager-KIFAdditions.h>
+#import <KIFFramework/UIAccessibilityElement-KIFAdditions.h>
+#import <KIFFramework/UIApplication-KIFAdditions.h>
+#import <KIFFramework/UIScrollView-KIFAdditions.h>
+#import <KIFFramework/UITableView-KIFAdditions.h>
+#import <KIFFramework/UITouch-KIFAdditions.h>
+#import <KIFFramework/UIWindow-KIFAdditions.h>
+#import <KIFFramework/XCTestCase-KIFAdditions.h>
