4c57ccd07053e0f180ebbb2ddae802dd8f8f7e8d
Jordan Zucker <jordan.zucker@gmail.com>
2015-05-13 14:54:07 -0700

    Added ability to sleep app for specified duration.
    Need to add a test for this

--- Classes/KIFUITestActor.h
+++ Classes/KIFUITestActor.h
@@ -533,4 +533,10 @@ static inline KIFDisplacement KIFDisplacementForSwipingInDirection(KIFSwipeDirec
  */
 - (void)swipeRowAtIndexPath:(NSIndexPath *)indexPath inTableView:(UITableView *)tableView inDirection:(KIFSwipeDirection)direction;
 
+/*!
+ @abstract Backgrounds app using UIAutomation command, simulating pressing the Home button
+ @param duration Amount of time for a background event before the app becomes active again
+ */
+- (void)deactivateAppForDuration:(NSTimeInterval)duration;
+
 @end
--- Classes/UIAutomationHelper.h
+++ Classes/UIAutomationHelper.h
@@ -14,4 +14,6 @@
 
 + (void)acknowledgeSystemAlert;
 
++ (void)deactivateAppForDuration:(NSNumber *)duration;
+
 @end
