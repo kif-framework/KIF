796c0fbe1c8ecb204060c12b6f4169bd5f1420d9
Jordan Zucker <jordan.zucker@gmail.com>
2015-05-13 15:49:23 -0700

    Added a test for backgrounding.
    Might be better to move the test elsewhere, but put it in its own
    view controller for the sake of it not really fitting with
    anything else.

---
+++ Test Host/BackgroundViewController.m
@@ -0,0 +1,38 @@
+//
+//  BackgroundViewController.m
+//  KIF
+//
+//  Created by Jordan Zucker on 5/13/15.
+//
+//
+
+@interface BackgroundViewController : UIViewController
+@property (nonatomic, weak) IBOutlet UILabel *label;
+@end
+
+@implementation BackgroundViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleApplicationDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    self.label.isAccessibilityElement = YES;
+    self.label.text = @"Start";
+    self.label.accessibilityLabel = self.label.text;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)handleApplicationDidEnterBackground:(NSNotification *)notification {
+    self.label.text = @"Back";
+    self.label.accessibilityLabel = self.label.text;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
