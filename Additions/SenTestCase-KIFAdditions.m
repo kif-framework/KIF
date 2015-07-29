a065274475adde9a9bd67036d0d80a23b8b60a5b
Andrey Tolpeev <andrey.tolpeev@lazada.com>
2015-07-29 14:44:46 +0700

    Added screenshot saving upon failure when using Specta

--- Additions/SenTestCase-KIFAdditions.m
+++ Additions/SenTestCase-KIFAdditions.m
@@ -8,6 +8,7 @@
 
 #import "SenTestCase-KIFAdditions.h"
 #import "LoadableCategory.h"
+#import "UIApplication-KIFAdditions.h"
 
 MAKE_CATEGORIES_LOADABLE(SenTestCase_KIFAdditions)
 
@@ -16,6 +17,7 @@ MAKE_CATEGORIES_LOADABLE(SenTestCase_KIFAdditions)
 - (void)failWithException:(NSException *)exception stopTest:(BOOL)stop
 {
     if (stop) {
+        [self writeScreenshotForException:exception];
         [self raiseAfterFailure];
     }
     [self failWithException:exception];
@@ -28,6 +30,15 @@ MAKE_CATEGORIES_LOADABLE(SenTestCase_KIFAdditions)
     for (NSException *exception in exceptions) {
         [self failWithException:exception stopTest:(exception == lastException ? stop : NO)];
     }
+}
+
+- (void)writeScreenshotForException:(NSException *)exception;
+{
+#ifndef KIF_SENTEST
+    [[UIApplication sharedApplication] writeScreenshotForLine:[exception.userInfo[@"SenTestLineNumberKey"] unsignedIntegerValue] inFile:exception.userInfo[@"SenTestFilenameKey"] description:nil error:NULL];
+#else
+    [[UIApplication sharedApplication] writeScreenshotForLine:exception.lineNumber.unsignedIntegerValue inFile:exception.filename description:nil error:NULL];
+#endif
 }
 
 @end
