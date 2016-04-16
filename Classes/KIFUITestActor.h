267b4d6e4359b48700e6ed6d84c8ce86957bb082
Peter Steinberger <steipete@gmail.com>
2016-04-17 00:46:36 +0200

    Fixes label name for documentation

--- Classes/KIFUITestActor.h
+++ Classes/KIFUITestActor.h
@@ -238,7 +238,7 @@ typedef NS_ENUM(NSUInteger, KIFPullToRefreshTiming) {
 /*!
  @abstract Taps a stepper to either increment or decrement the stepper. Presumed that - (minus) to decrement is on the left.
  @discussion This will locate the left or right half of the stepper and perform a calculated click.
- @param label The accessibility identifier of the view to interact with.
+ @param accessibilityLabel The accessibility identifier of the view to interact with.
  @param stepperDirection The direction in which to change the value of the stepper (KIFStepperDirectionIncrement | KIFStepperDirectionDecrement)
  */
 -(void)tapStepperWithAccessibilityLabel:(NSString *)accessibilityLabel increment:(KIFStepperDirection)stepperDirection;
