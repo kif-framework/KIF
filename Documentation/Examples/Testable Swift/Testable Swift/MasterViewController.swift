0d1328559990e114b7193ca53876cd6c757c72f9
Christian Steffens <cs@hibento.de>
2017-03-02 18:35:38 +0100

    * Testable Swift Project updated to Swift-3.0 and Xcode-8 Project-Setting (#950)
    * TableView filled from programmaticly
    + Adding Test for AccessibilityIdentifiers

--- Documentation/Examples/Testable Swift/Testable Swift/MasterViewController.swift
+++ Documentation/Examples/Testable Swift/Testable Swift/MasterViewController.swift
@@ -10,10 +10,76 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let cell = tableView.cellForRowAtIndexPath(indexPath)
-        if let text = cell?.textLabel?.text {
+    
+    let cellReuseIdentifier: String = "KIFCellReuseIdentifier"
+    let numberOfSections = 1
+    let numberOfRowsInSection1 = 3
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRowsInSection1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Colors"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellReuseIdentifier)
+        }
+        
+        cell?.isAccessibilityElement = true
+        
+        switch indexPath.row {
+        case 0:
+            cell?.textLabel?.text = "Red Color"
+            cell?.textLabel?.textColor = UIColor.red
+            cell?.accessibilityIdentifier = "Red Cell Identifier"
+            cell?.accessibilityLabel = "Red Cell Label"
+        case 1:
+            cell?.textLabel?.text = "Green Color"
+            cell?.textLabel?.textColor = UIColor.green
+            cell?.accessibilityIdentifier = "Green Cell Identifier"
+            cell?.accessibilityLabel = "Green Cell Label"
+        case 2:
+            cell?.textLabel?.text = "Blue Color"
+            cell?.textLabel?.textColor = UIColor.blue
+            cell?.accessibilityIdentifier = "Blue Cell Identifier"
+            cell?.accessibilityLabel = "Blue Cell Label"
+        default:
+            print("Unknown Row \(indexPath.row)")
+        }
+
+        return cell!
+
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let cell = tableView.cellForRow(at: indexPath) else {
+            print("Cell at \(indexPath) not found")
+            return
+        }
+        
+        print("Tapped Cell \(cell.textLabel?.text) with AccessibilityLabel \(cell.accessibilityLabel) and AccessibilityIdentifier \(cell.accessibilityIdentifier)")
+
+        if let text = cell.textLabel?.text {
             navigationItem.title = "Selected: " + text
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
     }
 }
--- Documentation/Examples/Testable Swift/Testable SwiftTests/SimpleSwiftTest.swift
+++ Documentation/Examples/Testable Swift/Testable SwiftTests/SimpleSwiftTest.swift
@@ -12,18 +12,25 @@ import XCTest
 
 
 extension XCTestCase {
-    func tester(file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+    func tester(_ file : String = #file, _ line : Int = #line) -> KIFUITestActor {
         return KIFUITestActor(inFile: file, atLine: line, delegate: self)
     }
 
-    func system(file : String = __FILE__, _ line : Int = __LINE__) -> KIFSystemTestActor {
+    func system(_ file : String = #file, _ line : Int = #line) -> KIFSystemTestActor {
         return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
     }
 }
 
 class SimpleSwiftTest: KIFTestCase {
-    func testGreen() {
-        tester().tapViewWithAccessibilityLabel("Green")
-        tester().waitForViewWithAccessibilityLabel("Selected: Green")
+    
+    func testGreenCellWithIdentifier() {
+        tester().tapView(withAccessibilityIdentifier: "Green Cell Identifier")
+        tester().waitForView(withAccessibilityIdentifier: "Selected: Green Color")
+    }
+    
+    func testBlueCellWithLabel() {
+        tester().tapView(withAccessibilityLabel: "Blue Cell Label")
+        tester().waitForView(withAccessibilityLabel: "Selected: Blue Color")
+
     }
 }
