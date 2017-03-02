0c9b1bf605d2410851720b93cbd8dd3ce9c25df1
Christian Steffens <cs@hibento.de>
2017-03-02 10:45:37 +0100

    * Testable Swift Project updated to Swift-3.0 and Xcode-8 Project-Setting * TableView filled from programmaticly + Adding Test for AccessibilityIdentifiers

--- Documentation/Examples/Testable Swift/Testable SwiftTests/Testable_SwiftTests.swift
+++ Documentation/Examples/Testable Swift/Testable SwiftTests/Testable_SwiftTests.swift
@@ -29,7 +29,7 @@ class Testable_SwiftTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
