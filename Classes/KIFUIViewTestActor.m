bd3d231e96f3c9f0db09d893743a8fcdee7b0e19
Justin Martin <jmartin@squareup.com>
2017-08-15 17:42:30 -0700

    Add a safety check that prevents using expectedResult without validating the entered text

--- Classes/KIFUIViewTestActor.m
+++ Classes/KIFUIViewTestActor.m
@@ -287,6 +287,10 @@ NSString *const inputFieldTestString = @"Testing";
 
 - (void)enterText:(NSString *)text expectedResult:(NSString *)expectedResult;
 {
+    if (!self.validateEnteredText && expectedResult) {
+        [self failWithMessage:@"Can't supply an expectedResult string if `validateEnteredText` is NO."];
+    }
+
     @autoreleasepool {
         KIFUIObject *found = [self _predicateSearchWithRequiresMatch:YES mustBeTappable:NO];
         [self.actor enterText:text intoElement:found.element inView:found.view expectedResult:expectedResult];
