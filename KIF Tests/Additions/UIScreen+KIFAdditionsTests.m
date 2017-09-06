f49139050b365e83a59c216f625d956bbb8f2cea
0xpablo <0xpablo@users.noreply.github.com>
2017-09-06 12:53:23 +0200

    Make majorSwipeDisplacement smaller
    On iOS 11, when performing a "full swipe" on a TableView cell, the first action is performed by default.
    
    The `majorSwipeDisplacement` is currently used for the amount of displacement perform when swiping a cell and it seems that 0.625 is too much and performs the first action automatically.

--- KIF Tests/Additions/UIScreen+KIFAdditionsTests.m
+++ KIF Tests/Additions/UIScreen+KIFAdditionsTests.m
@@ -38,22 +38,22 @@
 
 @implementation UIScreen_KIFAdditionsTests
 
-- (void)test_majorSwipeDisplacement_ScreenSizeEqualTo320Pts_Returns200Pts {
-    CGRect iPhone5PortraitBounds = CGRectMake(0, 0, 320, 468);
+- (void)test_majorSwipeDisplacement_ScreenSizeEqualTo320Pts_Returns160Pts {
+    CGRect iPhone5PortraitBounds = CGRectMake(0, 0, 320, 568);
     CustomBoundsUIScreen *screen = [[CustomBoundsUIScreen alloc] initWithBounds:iPhone5PortraitBounds];
     
     CGFloat actual = screen.majorSwipeDisplacement;
     
-    XCTAssertEqual(200, actual);
+    XCTAssertEqual(160, actual);
 }
 
-- (void)test_majorSwipeDisplacement_ScreenSizeEqualTo414Pts_Returns258Point75Pts {
-    CGRect iPhone5PortraitBounds = CGRectMake(0, 0, 414, 736);
-    CustomBoundsUIScreen *screen = [[CustomBoundsUIScreen alloc] initWithBounds:iPhone5PortraitBounds];
+- (void)test_majorSwipeDisplacement_ScreenSizeEqualTo414Pts_Returns207Pts {
+    CGRect iPhone6PlusPortraitBounds = CGRectMake(0, 0, 414, 736);
+    CustomBoundsUIScreen *screen = [[CustomBoundsUIScreen alloc] initWithBounds:iPhone6PlusPortraitBounds];
     
     CGFloat actual = screen.majorSwipeDisplacement;
     
-    XCTAssertEqual(258.75, actual);
+    XCTAssertEqual(207, actual);
 }
 
 @end
