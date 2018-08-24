b1be601e9dfbe033288eb7da6b607f996965f094
weixi1 <weixi1@staff.weibo.com>
2018-08-24 15:24:19 +0800

    Revert "tableviewcell search method modify"
    This reverts commit bddfa45bc57cc24e9a3b9852b18bf152788df46f.

--- Additions/UIApplication-KIFAdditions.m
+++ Additions/UIApplication-KIFAdditions.m
@@ -115,11 +115,9 @@ static const void *KIFRunLoopModesKey = &KIFRunLoopModesKey;
 {
     NSMutableArray *windows = self.windows.mutableCopy;
     UIWindow *keyWindow = self.keyWindow;
-//    keep searching key window first
-    if ([windows containsObject:keyWindow]) {
-        [windows removeObject:keyWindow];
+    if (![windows containsObject:keyWindow]) {
+        [windows addObject:keyWindow];
     }
-    [windows addObject:keyWindow];
     return windows;
 }
 
