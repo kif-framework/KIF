9a3401b43982e2ddaeb859228e54cc833e7de6e4
Daniel Levy <me@danl3v.com>
2020-06-23 17:46:21 -0700

    Fix compilation issue for Xcode 12

--- Additions/XCTestCase-KIFAdditions.m
+++ Additions/XCTestCase-KIFAdditions.m
@@ -34,7 +34,18 @@ static inline void Swizzle(Class c, SEL orig, SEL new)
 {
     self.continueAfterFailure = YES;
 
-    [self recordFailureWithDescription:exception.description inFile:exception.userInfo[@"FilenameKey"] atLine:[exception.userInfo[@"LineNumberKey"] unsignedIntegerValue] expected:NO];
+    NSString *filePath = exception.userInfo[@"FilenameKey"];
+    NSInteger lineNumber = [exception.userInfo[@"LineNumberKey"] unsignedIntegerValue];
+    XCTSourceCodeLocation *location = [[XCTSourceCodeLocation alloc] initWithFilePath:filePath
+                                                                           lineNumber:lineNumber];
+    XCTSourceCodeContext *context = [[XCTSourceCodeContext alloc] initWithLocation:location];
+    XCTIssue *issue = [[XCTIssue alloc] initWithType:XCTIssueTypeAssertionFailure
+                                  compactDescription:exception.description
+                                 detailedDescription:nil
+                                   sourceCodeContext:context
+                                     associatedError:nil
+                                         attachments:@[]];
+    [self recordIssue:issue];
 
     if (stop) {
         [self writeScreenshotForException:exception];
@@ -122,7 +133,19 @@ static inline void Swizzle(Class c, SEL orig, SEL new)
     if (![[arg3 name] isEqualToString:@"KIFFailureException"]) {
         [self KIF_recordUnexpectedFailureForTestRun:arg1 description:arg2 exception:arg3];
     } else {
-        [arg1 recordFailureWithDescription:[NSString stringWithFormat:@"Test suite stopped on fatal error: %@", arg3.description] inFile:arg3.userInfo[@"FilenameKey"] atLine:[arg3.userInfo[@"LineNumberKey"] unsignedIntegerValue] expected:NO];
+        NSString *description = [NSString stringWithFormat:@"Test suite stopped on fatal error: %@", arg3.description];
+        NSString *filePath = arg3.userInfo[@"FilenameKey"];
+        NSInteger lineNumber = [arg3.userInfo[@"LineNumberKey"] unsignedIntegerValue];
+        XCTSourceCodeLocation *location = [[XCTSourceCodeLocation alloc] initWithFilePath:filePath
+                                                                               lineNumber:lineNumber];
+        XCTSourceCodeContext *context = [[XCTSourceCodeContext alloc] initWithLocation:location];
+        XCTIssue *issue = [[XCTIssue alloc] initWithType:XCTIssueTypeAssertionFailure
+                                      compactDescription:description
+                                     detailedDescription:nil
+                                       sourceCodeContext:context
+                                         associatedError:nil
+                                             attachments:@[]];
+        [arg1 recordIssue:issue];
     }
 }
 
