11ab82fc91b4ceb2898a1a181ad9be68be91b196
Daniel Levy <me@danl3v.com>
2020-06-24 11:19:52 -0700

    PR Feedback

--- Additions/XCTestCase-KIFAdditions.m
+++ Additions/XCTestCase-KIFAdditions.m
@@ -34,6 +34,7 @@ static inline void Swizzle(Class c, SEL orig, SEL new)
 {
     self.continueAfterFailure = YES;
 
+#ifdef __IPHONE_14_0
     NSString *filePath = exception.userInfo[@"FilenameKey"];
     NSInteger lineNumber = [exception.userInfo[@"LineNumberKey"] unsignedIntegerValue];
     XCTSourceCodeLocation *location = [[XCTSourceCodeLocation alloc] initWithFilePath:filePath
@@ -46,6 +47,9 @@ static inline void Swizzle(Class c, SEL orig, SEL new)
                                      associatedError:nil
                                          attachments:@[]];
     [self recordIssue:issue];
+#else
+    [self recordFailureWithDescription:exception.description inFile:exception.userInfo[@"FilenameKey"] atLine:[exception.userInfo[@"LineNumberKey"] unsignedIntegerValue] expected:NO];
+#endif
 
     if (stop) {
         [self writeScreenshotForException:exception];
@@ -133,6 +137,7 @@ static inline void Swizzle(Class c, SEL orig, SEL new)
     if (![[arg3 name] isEqualToString:@"KIFFailureException"]) {
         [self KIF_recordUnexpectedFailureForTestRun:arg1 description:arg2 exception:arg3];
     } else {
+#ifdef __IPHONE_14_0
         NSString *description = [NSString stringWithFormat:@"Test suite stopped on fatal error: %@", arg3.description];
         NSString *filePath = arg3.userInfo[@"FilenameKey"];
         NSInteger lineNumber = [arg3.userInfo[@"LineNumberKey"] unsignedIntegerValue];
@@ -146,6 +151,9 @@ static inline void Swizzle(Class c, SEL orig, SEL new)
                                          associatedError:nil
                                              attachments:@[]];
         [arg1 recordIssue:issue];
+#else
+        [arg1 recordFailureWithDescription:[NSString stringWithFormat:@"Test suite stopped on fatal error: %@", arg3.description] inFile:arg3.userInfo[@"FilenameKey"] atLine:[arg3.userInfo[@"LineNumberKey"] unsignedIntegerValue] expected:NO];
+#endif
     }
 }
 
