a12131b68f3acc23f29f7c656f951631605701cf
Pablo Bartolome <pablobartolome@gmail.com>
2021-03-25 10:32:23 +0100

    Change test implementation to make codeclimate happier

--- Documentation/Examples/SPMIntegration/SPMIntegrationTests/SPMIntegrationTests.swift
+++ Documentation/Examples/SPMIntegration/SPMIntegrationTests/SPMIntegrationTests.swift
@@ -11,17 +11,13 @@ import KIF
 
 class SPM_IntegrationTests: KIFTestCase {
     func testGreenCellWithIdentifier() {
-        viewTester().usingLabel("Tap Me").tap()
-        viewTester().usingLabel("Done").waitForView()
+        uiViewTestActor.usingLabel("Tap Me").tap()
+        uiViewTestActor.usingLabel("Done").waitForView()
     }
 }
 
 private extension XCTestCase {
-    func viewTester(_ file : String = #file, _ line : Int = #line) -> KIFUIViewTestActor {
-        return KIFUIViewTestActor(inFile: file, atLine: line, delegate: self)
-    }
-
-    func system(_ file : String = #file, _ line : Int = #line) -> KIFSystemTestActor {
-        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    var uiViewTestActor: KIFUIViewTestActor {
+        KIFUIViewTestActor(inFile: #file, atLine: #line, delegate: self)
     }
 }
