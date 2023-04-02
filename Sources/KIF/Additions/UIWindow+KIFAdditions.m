655485afd55b7fb79ce8c375ff08002d8171a05f
steve-sun-me <117430445+steve-sun-me@users.noreply.github.com>
2023-04-02 15:07:55 -0700

    Set animation speed for all windows

---
+++ Sources/KIF/Additions/UIWindow+KIFAdditions.m
@@ -0,0 +1,37 @@
+//
+//  UIWindow+KIFAdditions.m
+//  KIF
+//
+//  Created by Steve Sun on 2023-04-02.
+//
+
+#import "UIWindow+KIFAdditions.h"
+#import "UIApplication-KIFAdditions.h"
+#import <objc/runtime.h>
+
+@implementation UIWindow (KIFAdditions)
+
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+
+        SEL originalSelector = @selector(becomeKeyWindow);
+        SEL swizzledSelector = @selector(swizzle_becomeKeyWindow);
+
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    });
+}
+
+- (void)swizzle_becomeKeyWindow
+{
+    [self swizzle_becomeKeyWindow];
+    self.layer.speed = [UIApplication sharedApplication].animationSpeed;
+    NSLog(@"Here");
+}
+
+@end
