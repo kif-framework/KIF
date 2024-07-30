28d5913425d823aeadcbc5140e4ad03c8f2d402b
Alex Odawa <aodawa@squareup.com>
2024-07-30 20:17:06 +0200

    using xcode version instead of minimum deployment target

--- Sources/KIF/Additions/UIView-Debugging.m
+++ Sources/KIF/Additions/UIView-Debugging.m
@@ -136,7 +136,7 @@
         case UIDatePickerModeCountDownTimer:
             printf(" UIDatePickerModeCountDownTimer");
             break;
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 17040
+#if __clang_major__ >= 15 // Xcode 15 or better. This might still error on xcode versions between 15.0 and 15.3, but that much harder to check for as they all use the same version of clang.
         case UIDatePickerModeYearAndMonth:
             printf(" UIDatePickerModeYearAndMonth");
             break;
