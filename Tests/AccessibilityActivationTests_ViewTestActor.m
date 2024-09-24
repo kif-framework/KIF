0213228e14b656c540ab1251f316c7e0fc5fd472
Alex Odawa <aodawa@squareup.com>
2024-09-24 22:11:34 +0200

    take the accessibilityActivation point into consideration when tapping elements (#1305)

--- Tests/AccessibilityActivationTests_ViewTestActor.m
+++ Tests/AccessibilityActivationTests_ViewTestActor.m
@@ -6,6 +6,7 @@
 //
 
 #import <KIF/KIF.h>
+#import <UIKit/UIAccessibility.h>
 
 @interface AccessibilityActivateTests_ViewTestActor : KIFTestCase
 @end
@@ -31,6 +32,19 @@
     [[viewTester usingLabel:@"AccessibilitySwitch"] setSwitchOn:false];
     [[viewTester usingLabel:@"AccessibilityView"] performAccessibilityActivateWithExpectedResult: NO];
     [[viewTester usingValue:@"Activated: 2"] waitForView];
+}
+
+- (void)testAccessibilityActiationPoint
+{
+    UIView *view = [[viewTester usingValue: @"Awaiting activation or tap"] waitForView];
+    [view setAccessibilityActivationPoint: [view.window convertPoint:CGPointMake(25.0, 50.0) fromView:view]];
+    
+    [[viewTester usingLabel: @"AccessibilityView"] tap];
+    [[viewTester usingValue:@"Tapped - x:25.0000, y:50.0000"] waitForView];
+    
+    [view setAccessibilityActivationPoint: [view.window convertPoint:CGPointMake(50.0, 25.0) fromView:view]];
+    [[viewTester usingLabel: @"AccessibilityView"] tap];
+    [[viewTester usingValue:@"Tapped - x:50.0000, y:25.0000"] waitForView];
 }
 
 @end
