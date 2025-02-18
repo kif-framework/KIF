11f53f8285943e8b476e35303f9a02df986952fc
Bartłomiej Włodarczak <bartlomiej.wlodarczak@outlook.com>
2025-02-18 18:17:06 +0100

    Fix LandscapeTests and LandscapeTests_ViewTestActor failing on iPadOS

--- Tests/LandscapeTests_ViewTestActor.m
+++ Tests/LandscapeTests_ViewTestActor.m
@@ -33,7 +33,15 @@
 - (void)testThatAlertViewsCanBeTappedInLandscape
 {
     [viewTester tapRowInTableViewAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:1]];
-    [[viewTester usingLabel:@"Cancel"] tap];
+    
+    if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPhone) {
+        [[viewTester usingLabel:@"Cancel"] tap];
+    } else {
+        /* On iPadOS the UIAlertController is displayed as a popup over table view cell, there's no "Cancel" button.
+         It can be dismissed by tapping anywhere on the screen.
+         */
+        [viewTester tapRowInTableViewAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:1]];
+    }
     [[viewTester usingLabel:@"Continue"] tap];
     [[viewTester usingLabel:@"Message"] waitForAbsenceOfView];
 }
